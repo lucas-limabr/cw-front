46858ddb6e5ab672157620a49dbd8b25ff34c8a4
Lucas Lima <130606633+lucas-limabr@users.noreply.github.com>
2024-11-26 18:40:38 -0300

    definindo rotas e views iniciais

---
+++ Codigo/Concessweb/cw-app/src/routes.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+
+import CadastroCliente from './views/listagem-cliente';
+import ListagemCliente from './views/cadastro-cliente';
+
+function Rotas(props) {
+    return(
+        console.log()
+    )
+}
+
